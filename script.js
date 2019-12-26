2b67c97a0940ef9cccfd21310eb4bcf6c0e49b62
raphyuki <raph.yuki@gmail.com>
2019-12-26 19:32:24 -0200

    stats dropdown menus return values

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 var stat_points = 0;
+
+function getStrValue(){
+    var selectedValue = document.getElementById("str").value;
+    console.log(selectedValue);
+}
+
+function getAgiValue(){
+    var selectedValue = document.getElementById("agi").value;
+    console.log(selectedValue);
+}
+
+function getVitValue(){
+    var selectedValue = document.getElementById("vit").value;
+    console.log(selectedValue);
+}
+
+function getIntValue(){
+    var selectedValue = document.getElementById("int").value;
+    console.log(selectedValue);
+}
+
+function getDexValue(){
+    var selectedValue = document.getElementById("dex").value;
+    console.log(selectedValue);
+}
+
+function getLukValue(){
+    var selectedValue = document.getElementById("luk").value;
+    console.log(selectedValue);
+}
