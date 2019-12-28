297cd2d8f13bf8a95e42781248befff86b7b1724
raphyuki <raph.yuki@gmail.com>
2019-12-27 22:56:49 -0200

    added more accurate drop-down selection

--- script.js
+++ script.js
@@ -29,3 +29,70 @@ function getLukValue(){
     var selectedValue = document.getElementById("luk").value;
     console.log(selectedValue);
 }
+
+function createBaseDropdown(){
+    var select = document.getElementById("base");
+    for(var i=1;i<=185;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createJobDropdown(){
+    var select = document.getElementById("job");
+    for(var i=1;i<=65;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createStrDropdown(){
+    var select = document.getElementById("str");
+    for(var i=1;i<=130;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createAgiDropdown(){
+    var select = document.getElementById("agi");
+    for(var i=1;i<=130;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createVitDropdown(){
+    var select = document.getElementById("vit");
+    for(var i=1;i<=130;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createIntDropdown(){
+    var select = document.getElementById("int");
+    for(var i=1;i<=130;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createDexDropdown(){
+    var select = document.getElementById("dex");
+    for(var i=1;i<=130;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createLukDropdown(){
+    var select = document.getElementById("luk");
+    for(var i=1;i<=130;i++){
+        select.options[select.options.length] = new Option(i, i);
+    }
+}
+
+function createDropdowns(){
+    createBaseDropdown();
+    createJobDropdown();
+    createStrDropdown();
+    createAgiDropdown();
+    createVitDropdown();
+    createIntDropdown();
+    createDexDropdown();
+    createLukDropdown();
+}
